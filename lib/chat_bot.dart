8e1133c282de5eaed23712f224a176960b24a73a
charan15923 <charan15923@gmail.com>
2023-01-21 10:57:23 +0530

    init

---
+++ lib/chat_bot.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ChatBot extends StatefulWidget {
+  const ChatBot({super.key});
+
+  @override
+  State<ChatBot> createState() => _ChatBotState();
+}
+
+class _ChatBotState extends State<ChatBot> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
